c79c918b549d6eb316db50ed9f3b1e887f7381ce
Sean Lucius <seanlucius22@icloud.com>
2019-02-06 21:41:01 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(*arguments)
     arguments_list = arguments.each{ |argument| }
-    self.select{ |key, value| value == arguments }
+    self.select{ |key, value| arguments_list.includes? }
   end
 end
